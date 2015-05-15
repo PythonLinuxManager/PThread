3f2f0b2c5db7dd6edd6a88d79201a24418940748
admin <shaoqing.ren@yahoo.com>
2015-05-15 16:16:13 +0800

    线程2

--- Pthread02/Pthread2.py
+++ Pthread02/Pthread2.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-  
 # 有一个全局的计数num，每个线程获得这个计数器并做一些操作，然后将num+1
+# 没有锁机制时，下面代码会有问题，num值 不定，拿到的信息不同
 import threading
 import time
 
@@ -8,7 +9,7 @@ class Mythread(threading.Thread):
 	def run(self):
 		global num
 		num += 1
-		time.sleep(0.002)
+		time.sleep(0.001)
 		msg = self.name+' set num to '+str(num)
 		print (msg)
 
